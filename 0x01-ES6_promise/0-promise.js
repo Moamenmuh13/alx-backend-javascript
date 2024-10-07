e4b0f1fb1087c1ddaf63d75bb9b9b8930a5e3ced
Moamen Muhammad <mumenmuhammad13@gmail.com>
2024-10-07 11:31:45 +0300

    fix the confilct

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import { rejects } from "assert";
 
 export default function getResponseFromAPI(){
     return new Promise((resolve, reject)=> {});
 }
-=======
-export default function getResponseFromAPI() {
-    return new Promise(() => { });
-}
->>>>>>> 2f16a5c02927ba8d3b7e3ac263e6c11f5be610a5
